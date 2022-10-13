c7955cc3b507215996cddfaef0ab3cff755e4873
margadb <43590110+margadb@users.noreply.github.com>
2022-10-13 11:32:08 -0600

    Add files via upload

---
+++ sudoku.py
@@ -0,0 +1,70 @@
+import numpy as np
+import itertools
+from Tkinter import Tk, Canvas, Frame, Button, BOTH, TOP, BOTTOM
+
+grid = [[5,3,0,0,7,0,0,0,0],
+        [6,0,0,1,9,5,0,0,0],
+        [0,9,8,0,0,0,0,6,0],
+        [8,0,0,0,6,0,0,0,3],
+        [4,0,0,8,0,3,0,0,1],
+        [7,0,0,0,2,0,0,0,6],
+        [0,6,0,0,0,0,2,8,0],
+        [0,0,0,4,1,9,0,0,5],
+        [0,0,0,0,8,0,0,7,9]]
+
+class SudokuUI(Frame):
+
+    def __init__(self, parent, game):
+        self.game = game
+        self.parent = parent
+        Frame.__init__(self, parent)
+
+        self.row, self.col = 0,0
+        self.__initUI()
+
+    def __initUI(self):
+        self.parent.title("Sudoku")
+        self.pack(fill=BOTH, expand=1)
+        self.canvas = Canvas(self, width=WIDTH, height=HEIGHT)
+        self.canvas.pack(fill=BOTH, side=TOP)
+        clear_button = Button(self, text,"Clear Answers", command=self.__clear_answers)
+        clear_button.pack(fill=BOTH, side=BOTTOM)
+
+        self.__draw_grid()
+        self.__draw_puzzle()
+
+        self.canvas.bind("<Button-1>", self.__cell_clicked)
+        self.canvas.bind("<Key>", self.__key_pressed)
+
+    #def __draw_grid(self):
+
+
+def poss(y,x,n):
+    global grid
+    for i in range(0,9):
+        if grid[y][i] == n:
+            return False
+    for i in range(0,9):
+        if grid[i][x] == n:
+            return False
+    x0 = (x//3)*3
+    y0 = (y//3)*3
+    for i in range(0,3):
+        for j in range(0,3):
+            if grid[y0+i][x0+j] == n:
+                return False
+    return True
+
+def solve():
+    global grid
+    for y, x in itertools.product(range(9), range(9)):
+        if grid[y][x] == 0:
+            for n in range(1,10):
+                if poss(y,x,n):
+                    grid[y][x] = n
+                    solve()
+                    grid[y][x] = 0
+            return
+    print(np.matrix(grid))
+
+solve()
